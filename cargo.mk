0387c126ffa5245485841ef389f7d59cc62f97da
Alex Crichton <alex@alexcrichton.com>
2014-07-14 19:16:22 -0700

    Enable 32-bit builds for travis

--- cargo.mk
+++ cargo.mk
@@ -1,3 +1,9 @@
+ifneq ($(findstring i686,$(TARGET)),)
+export CC := $(CC) -m32
+endif
+
+export COPT := -fPIC
+
 all:
 	$(MAKE) -C src/civetweb lib BUILD_DIR=$(DEPS_DIR)
 	mv src/civetweb/libcivetweb.a $(DEPS_DIR)/
