39c92f30a867461a39a1cba7a1bdb9fb2e6798e5
Alex Crichton <alex@alexcrichton.com>
2014-11-06 09:31:42 -0800

    Update the build script

--- build.rs
+++ build.rs
@@ -14,8 +14,14 @@ fn main() {
                     .stderr(InheritFd(2))
                     .status().unwrap().success());
 
-    fs::rename(&Path::new("src/civetweb/libcivetweb.a"),
-               &Path::new(&dst).join("libcivetweb.a")).unwrap();
+    {
+        let src = Path::new("src/civetweb/libcivetweb.a");
+        let dst = Path::new(&dst).join("libcivetweb.a");
+        if fs::rename(&src, &dst).is_err() {
+            fs::copy(&src, &dst).unwrap();
+            fs::unlink(&src).unwrap();
+        }
+    }
 
     println!("cargo:rustc-flags=-L {} -l civetweb:static", dst);
 }
