ed3459ec2d0814a95c21daeb8761e7fdf7fc2c66
Louis Person <louis@person.guru>
2017-08-13 01:32:04 +0200

    Allow warnings when compiling, also don't build tests

--- civet-sys/build.rs
+++ civet-sys/build.rs
@@ -5,6 +5,8 @@ use cmake::Config;
 fn main() {
     let mut dst = Config::new("civetweb")
                          .define("CMAKE_BUILD_TYPE", "Release")
+                         .define("BUILD_TESTING", "OFF")
+                         .define("CIVETWEB_ALLOW_WARNINGS", "ON")
                          .build();
     dst.push("lib");
     println!("cargo:rustc-link-search=native={}", dst.display());
