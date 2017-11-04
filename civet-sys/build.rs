3852740c8f391df028e3c30e430fb9be4836b7af
Justin Geibel <jtgeibel@gmail.com>
2017-11-03 23:01:13 -0400

    Switch from cmake to cc

--- civet-sys/build.rs
+++ civet-sys/build.rs
@@ -1,14 +1,8 @@
-extern crate cmake;
-
-use cmake::Config;
+extern crate cc;
 
 fn main() {
-    let mut dst = Config::new("civetweb")
-                         .define("CMAKE_BUILD_TYPE", "Release")
-                         .define("BUILD_TESTING", "OFF")
-                         .define("CIVETWEB_ALLOW_WARNINGS", "ON")
-                         .build();
-    dst.push("lib");
-    println!("cargo:rustc-link-search=native={}", dst.display());
-    println!("cargo:rustc-link-lib=static=civetweb");
+    cc::Build::new()
+        .file("civetweb/src/civetweb.c")
+        .include("civetweb/include")
+        .compile("civetweb");
 }
